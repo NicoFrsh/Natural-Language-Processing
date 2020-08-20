4918134e7fdd8c550fac62e7e2e0ec30753de466
NicoFrsh <nfrisch@unal.edu.co>
2020-08-20 17:59:46 -0500

    Write sample files to use them through analysis

--- Analysis.R
+++ Analysis.R
@@ -1,8 +1,66 @@
 # start connection with file
-con <- file("https://www.dropbox.com/s/v2085oc901rx0xf/en_US.blogs.txt?raw=1", "r")
-# read some lines
-data <- readLines(con, 20)
-close(con)
+conBlogs <- file("https://www.dropbox.com/s/v2085oc901rx0xf/en_US.blogs.txt?raw=1", "r")
+# read all lines and then sample 10%
+set.seed(1234)
+blogs <- readLines(con)
+blogsSample <- blogs[rbinom(length(blogs)*0.1, length(blogs), 0.5)]
+writeLines(blogsSample, con = "blogsSample.txt")
+close(conBlogs)
+
+conNews <- file("https://www.dropbox.com/s/pworwsmh2nhlub3/en_US.news.txt?raw=1", "r")
+news <- readLines(conNews)
+set.seed(1235)
+newsSample <- news[rbinom(length(news)*0.1, length(news), 0.5)]
+writeLines(newsSample, con = "newsSample.txt")
+close(conNews)
+
+conTwitter <- file("https://www.dropbox.com/s/2ki49yjvuqadz16/en_US.twitter.txt?raw=1", "r")
+twitter <- readLines(conTwitter)
+set.seed(1236)
+twitterSample <- twitter[rbinom(length(twitter)*0.1, length(twitter), 0.5)]
+writeLines(twitterSample, con = "twitterSample.txt")
+close(conTwitter)
+
+# clean workspace
+rm(blogs, news, twitter)
+
+# now we can always load the exact same samples
+conBlogs <- file("blogsSample.txt", "r")
+blogsSample <- readLines(conBlogs)
+close(conBlogs)
+
+
+# clean blogsSample
+# remove special characters
+# TODO: Write as function 'cleanText' or so
+gsub("[[:punct:]]", "", blogsSample)
+gsub("[^a-zA-Z0-9]", "", blogsSample)
+
+# everything to lower case
+tolower(blogsSample)
+# remove numbers
+
+
+
+# function for profanity filtering
+profanityFilter <- function(wordList){
+        
+        # read list with profanity words
+        con <- file("https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/3b70dd644cec678ddc43da88d30034add22897ef/google_twunter_lol", "r")
+        profList <- readLines(con)
+        close(con)
+        
+        library(stringr)
+        
+        str_replace_all(wordList, profList, replacement = NA_character_)
+        
+        wordList
+        
+}
+
+profanityFilter(blogsSample)
+
+
 
 # creating function that takes a file as input and
 # returns a tokenized version of it
@@ -12,21 +70,23 @@ tokenize <- function(file){
         file
 }
 
-# tokenize data
-data <- tokenize(data)
+# tokenize blogsSample
+blogsSample <- tokenize(blogsSample)
 
 
-data
+blogsSample
 
-# inspect data
-str(data)
-class(data)
-data[1]
 
-# use tokenizers package to tokenize the data into single words
-library(tokenizers)
+# test profanityFilter function
+con <- file("https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/3b70dd644cec678ddc43da88d30034add22897ef/google_twunter_lol", "r")
+profList <- readLines(con)
+close(con)
 
-data_token <- tokenize_words(data)
+text <- "I hate this motherfucking bitch ass guy."
 
-data_token[[1]]
+profanityFilter(text)
+text
 
+str_replace_all(text, profList, replacement = NA_character_)
+
+text
