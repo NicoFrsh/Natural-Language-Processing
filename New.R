5590a2b28c35d0caf1e05acbf9814ee05f766e3b
NicoFrsh <nfrisch@unal.edu.co>
2020-08-26 13:26:04 -0500

    Some exploratory analysis

--- New.R
+++ New.R
@@ -1,19 +1,23 @@
 # Analysis for complete corpus
+#Sys.setlocale("LC_CTYPE", "en_US.UTF-8")
 library(readtext)
 library(quanteda)
 
-all_data <- readtext(paste0(getwd(), "/Samples/*.txt"))
+all_data <- readtext(paste0(getwd(), "/Samples/*.txt"), encoding = "UTF-8")
 
-class(all_data)
+#class(all_data)
 
 # create a corpus 
 corp <- corpus(all_data)
-print(corp)
+#print(corp)
+
+# remove non utf-8 characters
+texts(corp) <- iconv(texts(corp), from = "UTF-8", to = "ASCII", sub = "")
 
 # perform data cleaning using the quanteda package
 # first create tokens object removing all non alphabetical characters
-tok <- tokens(corp, what = "fastestword", remove_punct = TRUE, remove_symbols = TRUE,
-              remove_numbers = TRUE, remove_url = TRUE)
+tok <- tokens(corp, what = "fasterword", remove_punct = TRUE, remove_symbols = TRUE,
+              remove_numbers = TRUE, remove_url = TRUE, split_hyphens = TRUE)
 
 # remove stop words
 tok <- tokens_select(tok, stopwords('english'),selection='remove')
@@ -21,7 +25,12 @@ tok <- tokens_select(tok, stopwords('english'),selection='remove')
 # to lower case
 tok <- tokens_tolower(tok)
 
-# TODO: remove profanity
+# remove profanity
+con <- file("https://gist.githubusercontent.com/ryanlewis/a37739d710ccdb4b406d/raw/3b70dd644cec678ddc43da88d30034add22897ef/google_twunter_lol", "r")
+profList <- readLines(con)
+close(con)
+
+tok <- tokens_select(tok, profList, selection = "remove")
 
 # convert to dfm
 dfm <- dfm(tok)
@@ -30,11 +39,88 @@ dfm <- dfm(tok)
 # key words in context
 head(kwic(tok, "school", window = 4))
 # top featured words
-topfeatures(dfm, 20)
+top20 <- topfeatures(dfm, 20)
+
+freq <- data.frame(feature = names(top20), frequency = top20)
+
+gg <- ggplot(freq, aes(x = reorder(feature, frequency), y = frequency)) + 
+        geom_bar(stat = "identity") +
+        coord_flip() +
+        labs(x = NULL, y = "Frequency", title = "Top 20 Features")
+
+gg
 
 # create n-grams
-tok_ngram <- tokens_ngrams(tok, n = 2:3)
+biGram <- tokens_ngrams(tok, n = 2)
 # 2-gram
-head(tok_ngram[[1]],30)
+head(biGram[[1]],30)
 # 3-gram
-head(tok_ngram[[2]],30)
+triGram <- tokens_ngrams(tok, n = 3)
+head(triGram[[1]],30)
+
+# not very useful, lets try selective n-grams
+# for example negation bigram
+just_bigram <- tokens_compound(tok, pattern = phrase("just *"))
+just_bigram_select <- tokens_select(just_bigram, pattern = phrase("just_*"))
+head(just_bigram_select[[1]],30)
+
+# generate skip-gram
+toks_skip <- tokens_ngrams(tok, n = 2, skip = 1:2)
+head(toks_skip[[1]], 30)
+
+# again, selective skip gram
+need_skipgram <- tokens_compound(tok, pattern = phrase("need *"))
+need_skipgram_select <- tokens_select(need_skipgram, pattern = phrase("need_*"))
+head(need_skipgram_select[[1]], 30)
+
+people_skipgram <-tokens_compound(tok, pattern = phrase("people *"))
+people_skipgram_select <- tokens_select(people_skipgram, pattern = phrase("people_*"))
+head(people_skipgram_select[[1]], 30)
+
+# now some stuff using the dfm
+# wordcloud
+textplot_wordcloud(dfm, random_order = FALSE, max_words = 150, rotation = 0.25,
+                   color = RColorBrewer::brewer.pal(8, "Dark2"))
+
+# convert the frequency count to a proportion within documents
+dfm_weight(dfm, scheme = "prop")
+dfm_weight(dfm, scheme = "count") # same as just print(dfm)
+dfm_tfidf(dfm)
+
+# create feature co-occurrence matrix
+fcm <- fcm(dfm_trim(dfm, min_termfreq = 200))
+topfeatures(fcm)
+feat <- names(topfeatures(fcm, 50))
+fcm_select <- fcm_select(fcm, pattern = feat, selection = "keep")
+
+# word embedding model
+library(text2vec)
+size <- log(colSums(dfm_select(dfm, feat, selection = "keep")))
+set.seed(144)
+textplot_network(fcm_select, min_freq = 0.8, vertex_size = size / max(size) * 3)
+
+# get word frequency grouped by document
+tstat_freq <- textstat_frequency(dfm, n = 10, groups = docnames(dfm))
+
+# plot in ggplot
+library(ggplot2)
+dfm %>%
+        textstat_frequency(n = 10, groups = docnames(dfm)) %>%
+        ggplot(aes(x = reorder(feature, frequency), y = frequency)) +
+        geom_bar(stat = "identity") +
+        facet_grid(. ~ group) +
+        coord_flip() +
+        labs(x = NULL, y = "Frequency")
+
+#### TEST 
+some_text <- "This. bullshit 12344 fucks don´t my head äâ bitch @nicofrsh #papijuancho."
+
+some_text <- iconv(some_text, from = "UTF-8", to = "ASCII", sub = "")
+
+# stringr before tokenizing
+#library(stringr)
+#some_text <- str_replace_all(some_text, "[^a-zA-Z\\s]", "")
+
+text_tok <- tokens(some_text, remove_numbers = TRUE, remove_symbols = TRUE, remove_punct = TRUE)
+text_tok <- tokens_select(text_tok, profList, selection = "remove")
+text_tok
