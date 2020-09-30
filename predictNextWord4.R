e3f559f2bfdd11aadd9bb43e87404e9ad7100d8e
NicoFrsh <nfrisch@unal.edu.co>
2020-09-30 11:06:36 -0500

    attempt indexing, no speed up though

--- predictNextWord4.R
+++ predictNextWord4.R
@@ -1,9 +1,117 @@
-# 4-gram prediction model
 # Stupid Backoff Algorithm (4-gram)
+# predictNextWord4 <- function(prevWords, unigrams, bigrams, trigrams, quadgrams, verbose = FALSE){
+#         
+#         # change to lower case and remove whitespace at beginning and end of string
+#         prevWords <- tolower(prevWords)
+#         prevWords <- stringr::str_trim(prevWords)
+#         
+#         n <- quanteda::ntoken(prevWords)
+#         if (verbose == TRUE) message(paste0("PrevWords has ", n, " words."))
+#         
+#         # only use at most trigrams to predict
+#         if (n > 3){
+#                 prevWords <- stringr::word(prevWords, start = -3, end = -1)
+#                 if (verbose == TRUE) {
+#                         message("Sentence too long, cutting to")
+#                         print(prevWords)
+#                 }
+#         }
+#         
+#         
+#         countDown <- quanteda::ntoken(prevWords) + 1
+#         # initialize data table
+#         nextWords <- data.table::data.table(feature = character(), frequency = numeric(), score = numeric())
+#         
+#         #### TODO: Add foundWord < 3 later
+#         i <- 1
+#         while(countDown >= 0){
+#                 
+#                 if (verbose == TRUE) message(paste0("Countdown: \t", countDown))
+#                 
+#                 if (i == 1 & quanteda::ntoken(prevWords) == 3){
+#                         if (verbose == TRUE) {
+#                                 message("Check quadgrams that contain:")
+#                                 print(prevWords)
+#                         }
+#                         nextWordsTemp <- quadgrams[startsWith(feature, paste0(prevWords, " "))]
+#                         nextWordsTemp <- head(nextWordsTemp, 10)
+#                         # cut off only the last word which is our next word
+#                         nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+#                         root <- trigrams[feature %chin% prevWords]
+#                         nextWordsTemp[, score := nextWordsTemp$frequency / root$frequency]
+#                         # extend data table without adding already found words
+#                         nextWords <- rbind(nextWords,
+#                                            dplyr::filter(nextWordsTemp, 
+#                                                          !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+#                         if (verbose == TRUE) print(nextWords)
+#                 }
+#                 
+#                 if (i == 2 & quanteda::ntoken(prevWords) == 2){
+#                         if (verbose == TRUE) {
+#                                 message("Check trigrams that contain:")
+#                                 print(prevWords)
+#                         }
+#                         nextWordsTemp <- trigrams[startsWith(feature, paste0(prevWords, " "))]
+#                         nextWordsTemp <- nextWordsTemp[1:10,]
+#                         # cut off only the last word which is our next word
+#                         nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+#                         root <- bigrams[feature %chin% prevWords]
+#                         nextWordsTemp[, score := 0.4 * nextWordsTemp$frequency / root$frequency]
+#                         # extend data table without adding already found words
+#                         nextWords <- rbind(nextWords, 
+#                                            dplyr::filter(nextWordsTemp, 
+#                                                          !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+#                         if (verbose == TRUE) print(nextWords)
+#                 }
+#                 
+#                 if (i == 3){
+#                         if (verbose == TRUE) {
+#                                 message("Check bigrams that contain:")
+#                                 print(prevWords)
+#                         }
+#                         nextWordsTemp <- bigrams[startsWith(feature, paste0(prevWords, " "))]
+#                         nextWordsTemp <- nextWordsTemp[1:10,]
+#                         # cut off only the last word which is our next word
+#                         nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+#                         root <- unigrams[feature %chin% prevWords]
+#                         nextWordsTemp[, score := 0.4 * 0.4 * nextWordsTemp$frequency / root$frequency]
+#                         # extend data table without adding already found words
+#                         nextWords <- rbind(nextWords, 
+#                                            dplyr::filter(nextWordsTemp, 
+#                                                          !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+#                         if (verbose == TRUE) print(nextWords)
+#                 }
+#                 
+#                 if (i == 4){
+#                         if (verbose == TRUE) message("Check most frequent unigrams")
+#                         nextWordsTemp <- unigrams[1:10,]
+#                         nextWordsTemp[, 
+#                                       score := 0.4 * 0.4 * 0.4 * nextWordsTemp$frequency / sum(unigrams[, frequency])]
+#                         # extend data table without adding already found words
+#                         nextWords <- rbind(nextWords, 
+#                                            dplyr::filter(nextWordsTemp, 
+#                                                          !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+#                         if (verbose == TRUE) print(nextWords)
+#                 }
+#                 
+#                 countDown <- countDown - 1
+#                 i <- i + 1
+#                 
+#                 prevWords <- sub(".*? ", "", prevWords)
+#                 
+#         }
+#         
+#         message("Algorithm terminated.")
+#         
+#         return(nextWords[order(-score)])
+#         
+# }
+
 predictNextWord4 <- function(prevWords, unigrams, bigrams, trigrams, quadgrams, verbose = FALSE){
         
-        # change to lower case
+        # change to lower case and remove whitespace at beginning and end of string
         prevWords <- tolower(prevWords)
+        prevWords <- stringr::str_trim(prevWords)
         
         n <- quanteda::ntoken(prevWords)
         if (verbose == TRUE) message(paste0("PrevWords has ", n, " words."))
@@ -18,84 +126,94 @@ predictNextWord4 <- function(prevWords, unigrams, bigrams, trigrams, quadgrams, 
         }
         
         
-        countDown <- quanteda::ntoken(prevWords) + 1
+        countDown <- quanteda::ntoken(prevWords)
         # initialize data table
         nextWords <- data.table::data.table(feature = character(), frequency = numeric(), score = numeric())
         
         #### TODO: Add foundWord < 3 later
-        i <- 1
-        while(countDown >= 0){
+        #i <- 1
+        while( countDown >= 0 & quanteda::ntoken(prevWords) > 0){
                 
                 if (verbose == TRUE) message(paste0("Countdown: \t", countDown))
                 
-                if (i == 1 & quanteda::ntoken(prevWords) == 3){
+                if (quanteda::ntoken(prevWords) == 3){
                         if (verbose == TRUE) {
                                 message("Check quadgrams that contain:")
                                 print(prevWords)
                         }
                         nextWordsTemp <- quadgrams[startsWith(feature, paste0(prevWords, " "))]
-                        nextWordsTemp <- head(nextWordsTemp, 20)
-                        # cut off only the last word which is our next word
-                        nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
-                        root <- trigrams[feature %chin% prevWords]
-                        nextWordsTemp[, score := nextWordsTemp$frequency / root$frequency]
-                        # extend data table without adding already found words
-                        nextWords <- rbind(nextWords,
-                                           dplyr::filter(nextWordsTemp, 
-                                                         !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
-                        if (verbose == TRUE) print(nextWords)
+                        
+                        if (!all(is.na(nextWordsTemp))){
+                                nextWordsTemp <- nextWordsTemp[order(-frequency)]
+                                nextWordsTemp <- head(nextWordsTemp, 10)
+                                # cut off only the last word which is our next word
+                                nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+                                root <- trigrams[feature %chin% prevWords]
+                                nextWordsTemp[, score := nextWordsTemp$frequency / root$frequency]
+                                # extend data table without adding already found words
+                                nextWords <- rbind(nextWords,
+                                                   dplyr::filter(nextWordsTemp, 
+                                                                 !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+                                if (verbose == TRUE) print(nextWords)
+                        }
                 }
                 
-                if (i == 2 & quanteda::ntoken(prevWords) == 2){
+                if (quanteda::ntoken(prevWords) == 2){
                         if (verbose == TRUE) {
                                 message("Check trigrams that contain:")
                                 print(prevWords)
                         }
                         nextWordsTemp <- trigrams[startsWith(feature, paste0(prevWords, " "))]
-                        nextWordsTemp <- nextWordsTemp[1:20,]
-                        # cut off only the last word which is our next word
-                        nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
-                        root <- bigrams[feature %chin% prevWords]
-                        nextWordsTemp[, score := 0.4 * nextWordsTemp$frequency / root$frequency]
-                        # extend data table without adding already found words
-                        nextWords <- rbind(nextWords, 
-                                           dplyr::filter(nextWordsTemp, 
-                                                         !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
-                        if (verbose == TRUE) print(nextWords)
+                        if (!all(is.na(nextWordsTemp))){
+                                nextWordsTemp <- nextWordsTemp[order(-frequency)]
+                                nextWordsTemp <- nextWordsTemp[1:10,]
+                                # cut off only the last word which is our next word
+                                nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+                                root <- bigrams[feature %chin% prevWords]
+                                nextWordsTemp[, score := 0.4 * nextWordsTemp$frequency / root$frequency]
+                                # extend data table without adding already found words
+                                nextWords <- rbind(nextWords, 
+                                                   dplyr::filter(nextWordsTemp, 
+                                                                 !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+                                if (verbose == TRUE) print(nextWords)
+                        }
                 }
                 
-                if (i == 3){
+                if (quanteda::ntoken(prevWords) == 1){
                         if (verbose == TRUE) {
                                 message("Check bigrams that contain:")
                                 print(prevWords)
                         }
                         nextWordsTemp <- bigrams[startsWith(feature, paste0(prevWords, " "))]
-                        nextWordsTemp <- nextWordsTemp[1:20,]
-                        # cut off only the last word which is our next word
-                        nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
-                        root <- unigrams[feature %chin% prevWords]
-                        nextWordsTemp[, score := 0.4 * 0.4 * nextWordsTemp$frequency / root$frequency]
-                        # extend data table without adding already found words
-                        nextWords <- rbind(nextWords, 
-                                           dplyr::filter(nextWordsTemp, 
-                                                         !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
-                        if (verbose == TRUE) print(nextWords)
+                        if (!all(is.na(nextWordsTemp))){
+                                nextWordsTemp <- nextWordsTemp[order(-frequency)]
+                                nextWordsTemp <- nextWordsTemp[1:10,]
+                                # cut off only the last word which is our next word
+                                nextWordsTemp <- nextWordsTemp[, .(feature = sub(".* ", "", feature), frequency)]
+                                root <- unigrams[feature %chin% prevWords]
+                                nextWordsTemp[, score := 0.4 * 0.4 * nextWordsTemp$frequency / root$frequency]
+                                # extend data table without adding already found words
+                                nextWords <- rbind(nextWords, 
+                                                   dplyr::filter(nextWordsTemp, 
+                                                                 !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
+                                if (verbose == TRUE) print(nextWords)
+                        }
                 }
                 
-                if (i == 4){
+                if (quanteda::ntoken(prevWords) == 1 & all(is.na(nextWords))){
                         if (verbose == TRUE) message("Check most frequent unigrams")
-                        nextWordsTemp <- unigrams[1:20,]
-                        nextWordsTemp[, 
-                                      score := 0.4 * 0.4 * 0.4 * nextWordsTemp$frequency / sum(unigrams[, frequency])]
+                        nextWordsTemp <- unigrams[1:10,]
                         # extend data table without adding already found words
                         nextWords <- rbind(nextWords, 
                                            dplyr::filter(nextWordsTemp, 
                                                          !feature %chin% intersect(nextWords$feature, nextWordsTemp$feature)))
                         if (verbose == TRUE) print(nextWords)
+                        
+                        break
                 }
                 
                 countDown <- countDown - 1
-                i <- i + 1
+                #i <- i + 1
                 
                 prevWords <- sub(".*? ", "", prevWords)
                 
